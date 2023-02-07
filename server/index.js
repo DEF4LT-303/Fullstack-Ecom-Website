8a6b2646d167b96d9e5785308f2a64ff3e1bfd8e
DEF4LT-303 <ryanrafi72@gmail.com>
2023-02-07 13:17:51 +0600

    routes and models

--- server/index.js
+++ server/index.js
@@ -3,12 +3,19 @@ const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv');
 
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 mongoose
   .connect(process.env.DB_CONNECT)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
